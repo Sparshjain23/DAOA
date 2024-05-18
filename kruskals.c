d3d3fc4c36caf5a5e272002416ff64ca4bd680a0
Sparshjain23 <152734111+Sparshjain23@users.noreply.github.com>
2024-05-18 06:34:11 +0530

    Add files via upload

---
+++ kruskals.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <limits.h>
+#define V 5
+#define INF 999
+
+int parent[V];
+
+int find(int i) 
+{
+	while (parent[i] != i) 
+    {
+		i = parent[i];
+	}
+	return i;
+}
+
+void union_set(int i, int j) 
+{
+	int a = find(i);
+	int b = find(j);
+	parent[a] = b;
+}
+
+void main() 
+{
+	int graph[V][V];
+    
+    printf("Enter the adjacency matrix for the graph (use %d for infinity):\n", INF);
+    for (int i = 0; i < V; i++) 
+    {
+        for (int j = 0; j < V; j++) 
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    int mincost = 0;
+
+	for (int i = 0; i < V; i++) 
+    {
+		parent[i] = i;
+	}
+	int edge_count = 0;
+
+	while (edge_count < V - 1) 
+    {
+		int min = INF;
+		int a = -1;
+		int b = -1;
+		for (int i = 0; i < V; i++) 
+        {
+			for (int j = 0; j < V; j++) 
+            {
+				if (find(i) != find(j) && graph[i][j] < min) 
+                {
+					min = graph[i][j];
+					a = i;
+					b = j;
+				}
+			}
+		}
+		union_set(a, b);
+		printf("Edge %d: (%d, %d) cost: %d\n", edge_count + 1, a, b, min);
+		edge_count += 1;
+		mincost += min;
+	}
+
+	printf("Minimum cost= %d\n", mincost);
+}
